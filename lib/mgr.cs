9b08cf437feda88ddab4546357cf745aecd7bf49
Michael Balloni <balloni.michael@yahoo.com>
2026-02-23 21:04:22 -0800

    Created mgr class, coded store function.

---
+++ lib/mgr.cs
@@ -0,0 +1,58 @@
+﻿using System.Reflection;
+using System.Security.Cryptography;
+using System.Text;
+
+using ICSharpCode.SharpZipLib.Zip;
+
+namespace lib
+{
+    public static class mgr
+    {
+        public static async Task<string?> store(string pwd, string filename, Stream file)
+        {
+            string key = BitConverter.ToString(RandomNumberGenerator.GetBytes(64));
+            string zip_file_path = getFilePath(key, pwd);
+
+            bool success = false;
+            try
+            {
+                using (var zip_file = File.Create(zip_file_path))
+                {
+                    using (var zip_file_stream = new ZipOutputStream(zip_file))
+                    {
+                        zip_file_stream.Password = pwd;
+                        await file.CopyToAsync(zip_file_stream);
+                    }
+                }
+                success = true;
+            }
+            catch (Exception exp)
+            {
+                Console.WriteLine("Store EXCEPTION:\n{0}", exp);
+            }
+
+            if (success)
+                return key;
+
+            try
+            {
+                File.Delete(zip_file_path);
+            }
+            catch { }
+
+            return null;
+        }
+
+        private static string getFilePath(string key, string pwd)
+        {
+            string filename = 
+                BitConverter.ToString(SHA512.HashData(Encoding.Unicode.GetBytes(key + pwd))) + ".dat";
+
+            string file_path = Path.Combine(filename, sm_dirPath);
+            return file_path;
+        }
+
+        private static string sm_dirPath =
+                Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().GetModules()[0].FullyQualifiedName ?? ""), "files");
+    }
+}
